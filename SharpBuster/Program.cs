f6b5461759b7d23a49227d5a8fd07eeaf9714b28
unknown <joshua.n.magri@gmail.com>
2020-08-31 18:03:08 -0500

    Added status code handling, proxy support, cookie support, basic auth support

--- SharpBuster/Program.cs
+++ SharpBuster/Program.cs
@@ -14,6 +14,13 @@ namespace SharpBuster
 {
     class Program
     {
+
+        public static class GlobalHttpHandler
+        {
+            public static HttpClientHandler clientHandler = new HttpClientHandler();
+
+        }
+        
         static async Task Main(string[] args)
         {
             CommandLineApplication commandLineApplication = new CommandLineApplication(throwOnUnexpectedArg: false);
@@ -28,10 +35,20 @@ namespace SharpBuster
                 "-e | --ext", "A comma separated list of extensions to append, ex: php,asp,aspx", CommandOptionType.SingleValue);
             CommandOption recursive = commandLineApplication.Option(
                 "-r | --recursive", "Perform a recurisve search", CommandOptionType.SingleValue);
+            CommandOption username = commandLineApplication.Option(
+                "--username", "Username for basic authentication", CommandOptionType.SingleValue);
+            CommandOption password = commandLineApplication.Option(
+                "--password", "Password for authentication", CommandOptionType.SingleValue);
+            CommandOption proxy = commandLineApplication.Option(
+                "--proxy", "Address of proxy to use, ex: http://127.0.0.1:8080", CommandOptionType.SingleValue);
+            CommandOption proxyCredentials = commandLineApplication.Option(
+                "--proxy-creds", "Credentials to use to authenticate to proxy, ex: username:password", CommandOptionType.SingleValue);
+            CommandOption cookie = commandLineApplication.Option(
+                "--cookie", "Cookie to use, ex: myCookie=value | If multiple cookies are being used, separate them with a comma", CommandOptionType.SingleValue);
             commandLineApplication.HelpOption("-h | --help");
             commandLineApplication.OnExecute(async () =>
             {
-                Console.WriteLine(@" #####  #     #    #    ######  ######  ######  #     #  #####  ####### ####### ######  
+            Console.WriteLine(@" #####  #     #    #    ######  ######  ######  #     #  #####  ####### ####### ######  
 #     # #     #   # #   #     # #     # #     # #     # #     #    #    #       #     # 
 #       #     #  #   #  #     # #     # #     # #     # #          #    #       #     # 
  #####  ####### #     # ######  ######  ######  #     #  #####     #    #####   ######  
@@ -40,40 +57,74 @@ namespace SharpBuster
  #####  #     # #     # #     # #       ######   #####   #####     #    ####### #     # 
 Author: @passthehashbrwn
 ");
-                string wordlistRead = "";
-                if(wordlist.HasValue() && wordlistURL.HasValue())
-                {
-                    Console.WriteLine("Can't use both wordlist and wordlisturl.");
-                    return 0;
-                }
-                if(wordlist.HasValue())
-                {
-                    wordlistRead = File.ReadAllText(wordlist.Value());
-                }
-                else if(wordlistURL.HasValue())
-                {
-                    wordlistRead = GetWordlist(wordlistURL.Value());
-                }
-                string[] wordlistSeparated = wordlistRead.Split("\n");
 
-                //if (extensions.HasValue() && !recursive.HasValue())
-                //{
-                 //   await RunExt(url.Value(), wordlistSeparated, extensions.Value().Split(","));
-                //}
-                if(extensions.HasValue() && !recursive.HasValue())
-                {
-                    
-                    await RunExt(url.Value(), wordlistSeparated, extensions.Value().Split(","));
-                }
-                else if(extensions.HasValue() && recursive.HasValue())
-                {
-                    await RunRecursive(url.Value(), wordlistSeparated, extensions.Value().Split(","));
-                }
-                else
-                {
-                    await RunNormal(url.Value(), wordlistSeparated);
-                }
+            string wordlistRead = "";
+            if (wordlist.HasValue() && wordlistURL.HasValue())
+            {
+                Console.WriteLine("Can't use both wordlist and wordlisturl.");
                 return 0;
+            }
+            if (wordlist.HasValue())
+            {
+                wordlistRead = File.ReadAllText(wordlist.Value());
+            }
+            else if (wordlistURL.HasValue())
+            {
+                wordlistRead = GetWordlist(wordlistURL.Value());
+            }
+            if ((username.HasValue() && !password.HasValue()) || (!username.HasValue() && password.HasValue()))
+            {
+                Console.WriteLine("Must provide both username and password");
+            }
+            if (username.HasValue() && password.HasValue())
+            {
+                GlobalHttpHandler.clientHandler.Credentials = new NetworkCredential(username.Value(), password.Value());
+            }
+            if (proxy.HasValue())
+            {
+                WebProxy myProxy = new WebProxy();
+                Uri myUri = new Uri(proxy.Value());
+                myProxy.Address = myUri;
+                if (proxyCredentials.HasValue())
+                {
+                    myProxy.Credentials = new NetworkCredential(proxyCredentials.Value().Split(":")[0], proxyCredentials.Value().Split(":")[1]);
+                }
+                GlobalHttpHandler.clientHandler.Proxy = myProxy;
+            }
+            if(cookie.HasValue())
+            {
+                CookieContainer myCookie = new CookieContainer();
+                string[] cookieSplit = cookie.Value().Split(",");
+                for(int i = 0; i < cookieSplit.Length; i++)
+                {
+                    string cookieName = cookieSplit[i].Split("=")[0];
+                    string cookieValue = cookieSplit[i].Split("=")[1];
+                    myCookie.Add(new Cookie(cookieName, cookieValue));
+                }
+                GlobalHttpHandler.clientHandler.CookieContainer = myCookie;
+            }
+            GlobalHttpHandler.clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            string[] wordlistSeparated = wordlistRead.Split("\n");
+
+            //if (extensions.HasValue() && !recursive.HasValue())
+            //{
+                //   await RunExt(url.Value(), wordlistSeparated, extensions.Value().Split(","));
+            //}
+            if(extensions.HasValue() && !recursive.HasValue())
+            {
+                    
+                await RunExt(url.Value(), wordlistSeparated, extensions.Value().Split(","));
+            }
+            else if(extensions.HasValue() && recursive.HasValue())
+            {
+                await RunRecursive(url.Value(), wordlistSeparated, extensions.Value().Split(","));
+            }
+            else
+            {
+                await RunNormal(url.Value(), wordlistSeparated);
+            }
+            return 0;
             });
             commandLineApplication.Execute(args);
             
@@ -81,10 +132,11 @@ Author: @passthehashbrwn
 
         public static async Task GetDirectory(string url, string directory)
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            
+            //HttpClientHandler clientHandler = new HttpClientHandler();
+            //clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
 
-            HttpClient client = new HttpClient(clientHandler);
+            HttpClient client = new HttpClient(GlobalHttpHandler.clientHandler);
             try
             {
                 var request = WebRequest.Create(url + "/" + directory);
@@ -93,12 +145,12 @@ Author: @passthehashbrwn
                                             request.EndGetResponse,
                                             null);
                 Console.Write(directory + ":");
-                Console.WriteLine(response.StatusCode);
+                HandleStatusCode(response);
 
             }
-            catch (System.Net.WebException ex)
+            catch (WebException ex)
             {
-                //Console.WriteLine("404");
+                
             }
         }
 
@@ -113,6 +165,43 @@ Author: @passthehashbrwn
             dataStream.Close();
             response.Close();
             return wordlist;
+        }
+
+        public static void HandleStatusCode(HttpWebResponse response)
+        {
+            var status = response.StatusCode;
+            switch(status)
+            {
+                case HttpStatusCode.OK:
+                    Console.WriteLine("200");
+                    break;
+                case HttpStatusCode.Forbidden:
+                    Console.WriteLine("403");
+                    break;
+                case HttpStatusCode.InternalServerError:
+                    Console.WriteLine("500");
+                    break;
+                case HttpStatusCode.MovedPermanently:
+                    Console.WriteLine("301");
+                    break;
+                case HttpStatusCode.Redirect:
+                    Console.Write("302 => ");
+                    Console.WriteLine(response.Headers.Get("Location"));
+                    break;
+                case HttpStatusCode.Unauthorized:
+                    Console.WriteLine("401");
+                    break;
+                case HttpStatusCode.NoContent:
+                    Console.WriteLine("204");
+                    break;
+                case HttpStatusCode.RedirectKeepVerb:
+                    Console.Write("307 => ");
+                    Console.WriteLine(response.Headers.Get("Location"));
+                    break;
+                default:
+                    Console.WriteLine(status.ToString());
+                    break;
+            }
         }
 
         public static async Task RunNormal(string url, string[] wordlist)
@@ -138,9 +227,9 @@ Author: @passthehashbrwn
         public static async Task RunRecursive(string url, string[] wordlist, string[] extensions)
         {
             List<string> recursiveList = new List<string>();
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-            HttpClient client = new HttpClient(clientHandler);
+            //HttpClientHandler clientHandler = new HttpClientHandler();
+            //clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client = new HttpClient(GlobalHttpHandler.clientHandler);
             for (int i = 0; i < wordlist.Length; i++)
             {
                 try 
@@ -151,7 +240,7 @@ Author: @passthehashbrwn
                                                 request.EndGetResponse,
                                                 null);
                     Console.Write(wordlist[i] + ":");
-                    Console.WriteLine(response.StatusCode);
+                    HandleStatusCode(response);
                     recursiveList.Add(wordlist[i]);
                     Console.WriteLine("[+] Adding directory to queue...");
                 }
@@ -169,8 +258,8 @@ Author: @passthehashbrwn
                                                     request.EndGetResponse,
                                                     null);
                         Console.Write(wordlist[i] + "." + extensions[j] + ":");
-                        Console.WriteLine(response.StatusCode);
-                        
+                        HandleStatusCode(response);
+
                     }
                     catch (System.Net.WebException ex)
                     {
@@ -208,7 +297,7 @@ Author: @passthehashbrwn
                                                         request.EndGetResponse,
                                                         null);
                             Console.Write(recursiveList[i] + "/" + wordlist[j] + ":");
-                            Console.WriteLine(response.StatusCode);
+                            HandleStatusCode(response);
                             recursiveList.Add(recursiveList[i] + "/" + wordlist[j]);
                             Console.WriteLine("Added {0} to queue...", recursiveList[i] + wordlist[j]);
 
@@ -227,7 +316,7 @@ Author: @passthehashbrwn
                                                             request.EndGetResponse,
                                                             null);
                                 Console.Write(recursiveList[i] + "/" + wordlist[j] + ":");
-                                Console.WriteLine(response.StatusCode);
+                                HandleStatusCode(response);
 
                             }
                             catch (System.Net.WebException ex)
