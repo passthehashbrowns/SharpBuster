0cf24c408071318e75f40b0233ba9f4bb1f035fc
unknown <joshua.n.magri@gmail.com>
2020-08-30 21:49:44 -0500

    Added support for extensions

--- SharpBuster/Program.cs
+++ SharpBuster/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.Extensions.CommandLineUtils;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SharpBuster
@@ -23,6 +26,8 @@ namespace SharpBuster
                 "--wordlisturl", "URL of wordlist to use to avoid writing to disk", CommandOptionType.SingleValue);
             CommandOption extensions = commandLineApplication.Option(
                 "-e | --ext", "A comma separated list of extensions to append, ex: php,asp,aspx", CommandOptionType.SingleValue);
+            CommandOption recursive = commandLineApplication.Option(
+                "-r | --recursive", "Perform a recurisve search", CommandOptionType.SingleValue);
             commandLineApplication.HelpOption("-h | --help");
             commandLineApplication.OnExecute(async () =>
             {
@@ -42,23 +47,19 @@ namespace SharpBuster
                     wordlistRead = GetWordlist(wordlistURL.Value());
                 }
                 string[] wordlistSeparated = wordlistRead.Split("\n");
-                if (extensions.HasValue())
+
+                //if (extensions.HasValue() && !recursive.HasValue())
+                //{
+                 //   await RunExt(url.Value(), wordlistSeparated, extensions.Value().Split(","));
+                //}
+                if(extensions.HasValue() && !recursive.HasValue())
                 {
-                    string[] csvExtensions = extensions.Value().Split(",");
-                    for (int i = 0; i < wordlistSeparated.Length; i++)
-                    {
-                        for (int j = 0; j < csvExtensions.Length; j++)
-                        {
-                            await GetDirectory(url.Value(), wordlistSeparated[i] + csvExtensions[j]);
-                        }
-                    }
+                    
+                    await RunExt(url.Value(), wordlistSeparated, extensions.Value().Split(","));
                 }
                 else
                 {
-                    for (int i = 0; i < wordlistSeparated.Length; i++)
-                    {
-                        await GetDirectory(url.Value(), wordlistSeparated[i]);
-                    }
+                    await RunNormal(url.Value(), wordlistSeparated);
                 }
                 return 0;
             });
@@ -89,7 +90,6 @@ namespace SharpBuster
             }
         }
 
-
         public static string GetWordlist(string url)
         {
             WebRequest request = WebRequest.Create(url);
@@ -101,6 +101,86 @@ namespace SharpBuster
             dataStream.Close();
             response.Close();
             return wordlist;
+        }
+
+        public static async Task RunNormal(string url, string[] wordlist)
+        {
+            for (int i = 0; i < wordlist.Length; i++)
+            {
+                await GetDirectory(url, wordlist[i]);
+            }
+        }
+
+        public static async Task RunExt(string url, string[] wordlist, string[] ext)
+        {
+            for (int i = 0; i < wordlist.Length; i++)
+            {
+                for(int j = 0; j < ext.Length; j++)
+                {
+                    await GetDirectory(url, wordlist[i] + "." + ext[j]);
+                }
+            }
+        }
+
+        public static async Task RunRecursive(string url, string[] wordlist, string[] extensions)
+        {
+            string[] recursiveList = new string[] { };
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            HttpClient client = new HttpClient(clientHandler);
+            for (int i = 0; i < wordlist.Length; i++)
+            {
+                for(int j = 0; j < extensions.Length; j++)
+                {
+                    try
+                    {
+                        var request = WebRequest.Create(url + "/" + wordlist[i] + "." + extensions[j]);
+                        var response = (HttpWebResponse)await Task.Factory
+                            .FromAsync<WebResponse>(request.BeginGetResponse,
+                                                    request.EndGetResponse,
+                                                    null);
+                        Console.Write(wordlist[i] + ":");
+                        Console.WriteLine(response.StatusCode);
+                        recursiveList.Append(wordlist[i] + "." + extensions[j]);
+                    }
+                    catch (System.Net.WebException ex)
+                    {
+                        //Console.WriteLine("404");
+                    }
+                }
+                
+            }
+
+            while(recursiveList.Any())
+            {
+                for(int i = 0; i < recursiveList.Length; i++)
+                {
+                    for(int j = 0; j < wordlist.Length; j++)
+                    {
+                        for(int k = 0; k < extensions.Length; k++)
+                        {
+                            try
+                            {
+                                var request = WebRequest.Create(url + "/" + recursiveList[i] + "/" + wordlist[j] + "." + extensions[k]);
+                                var response = (HttpWebResponse)await Task.Factory
+                                    .FromAsync<WebResponse>(request.BeginGetResponse,
+                                                            request.EndGetResponse,
+                                                            null);
+                                Console.Write(recursiveList[i] + "/" + wordlist[j] + ":");
+                                Console.WriteLine(response.StatusCode);
+                                recursiveList.Append(wordlist[j]);
+                                Console.WriteLine("Added {0} to queue...", recursiveList[i] + "/" + wordlist[j] + "." + extensions[k]);
+
+                            }
+                            catch (System.Net.WebException ex)
+                            {
+                                //Console.WriteLine("404");
+                            }
+                        }                        
+                    }
+                }
+            }
         }
 
     }
